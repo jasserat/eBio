c65907164ac03567fcf7df7f52bc10bbfda0da2f
jasserat <jasser.atouani@etudiant-fst.utm.tn>
2023-03-06 19:38:34 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const logger = require('morgan');
+const createError = require('http-errors');
+
+const mongoose = require('mongoose');
+const dbConfig = require('./DB/mongodb.json');
+
+const cxpFormRouter = require('./routes/cxpFormR');
+const orderRouter = require('./routes/orderR');
+const productRouter = require('./routes/productR');
+const userRouter = require('./routes/userR');
+const WasteFormRouter = require('./routes/WasteFormR');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/cxpForm', cxpFormRouter);
+app.use('/order', orderRouter);
+app.use('/product', productRouter);
+app.use('/user', userRouter);
+app.use('/wasteForm', WasteFormRouter);
+
+app.use((req, res, next)=>{
+  next(createError(404));
+});
+
+mongoose.set('strictQuery', true);
+mongoose.connect(dbConfig.mongo.uri);
+
+module.exports = app;
