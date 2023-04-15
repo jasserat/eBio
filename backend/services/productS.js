a631d61f421c457790e6a5b243459db8bcb75c0e
jasserat <jasser.atouani@etudiant-fst.utm.tn>
2023-04-15 11:19:24 +0100

    some updates

--- backend/services/productS.js
+++ backend/services/productS.js
@@ -1,5 +1,5 @@
 const product = require('../models/product');
-
+/*
 module.exports.addProduct = async (req, res) => {
     try {
         const newProduct = new product(req.body);
@@ -8,12 +8,37 @@ module.exports.addProduct = async (req, res) => {
     } catch (error) {
         res.status(400).json(error);
     }
+}*/
+
+// ajouter produit champs par champs
+module.exports.addProduct = async (req, res) => {
+    try {
+        const newProduct = new product({
+            name: req.body.name,
+            price: req.body.price,
+            description: req.body.description,
+            image: req.body.image,
+            category: req.body.category,
+            quantity: req.body.quantity,
+            farmer: req.body.farmer,
+            // rating: req.body.rating,
+            // reviews: req.body.reviews,
+            // date: req.body.date
+        });
+        const result = await newProduct.save();
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(400).json(error);
+    }
 }
 
+//list product sorted by price
 module.exports.listProduct = async (req, res) => {
     try {
-        const result = await product.find();
+        const result = await product.find().sort({price: 1});
         res.status(200).json(result);
+        console.log("success")
+        console.log(result)
     } catch (error) {
         res.status(400).json(error);
     }
@@ -28,15 +53,34 @@ module.exports.getProductById = async (req, res) => {
     }
 }
 
-//update product
+//update product champs par champs
 module.exports.editProduct = async (req, res) => {
     try{
-        const result = await product.findByIdAndUpdate(req.params.id, req.body);
+        const result = await product.findByIdAndUpdate(req.params.id, {
+            name: req.body.name,
+            price: req.body.price,
+            description: req.body.description,
+            image: req.body.image,
+            category: req.body.category,
+            quantity: req.body.quantity,
+            farmer: req.body.farmer,
+            // rating: req.body.rating,
+            // reviews: req.body.reviews,
+            // date: req.body.date
+        });
         res.status(200).json(result);
     } catch (error) {
         res.status(400).json(error);
     }
 }
+// module.exports.editProduct = async (req, res) => {
+//     try{
+//         const result = await product.findByIdAndUpdate(req.params.id, req.body);
+//         res.status(200).json(result);
+//     } catch (error) {
+//         res.status(400).json(error);
+//     }
+// }
 
 //delete product
 module.exports.deleteProduct = async (req, res) => {
